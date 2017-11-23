d4203c6deab86190b769bff4537702d6d37d7e46
Jorik <jorik@creektiger.ch>
2017-11-23 21:33:35 +0100

    no message

--- build.sbt
+++ build.sbt
@@ -8,15 +8,17 @@ version := "0.1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  "de.dfki.mary" % "marytts-lang-de" % "5.2" % "provided"
+  "de.dfki.mary" % "marytts-lang-de" % "5.2" % "provided",
+  "com.typesafe.akka" %% "akka-http" % "10.0.10",
+  "com.typesafe.akka" %% "akka-http-testkit" % "10.0.10" % Test
 )
 
 enablePlugins(JavaAppPackaging)
 
 scriptClasspath ~= (cp => Seq(
-    "../../mary",
-    "../../mary/lib",
-    "../../mary/lib/marytts-lang-de-5.2.jar",
-    "../../mary/lib/voice-dfki-pavoque-styles-5.2.jar",
-    "../../mary/lib/marytts-runtime-5.2-jar-with-dependencies.jar"
+  "../../mary",
+  "../../mary/lib",
+  "../../mary/lib/marytts-lang-de-5.2.jar",
+  "../../mary/lib/voice-dfki-pavoque-styles-5.2.jar",
+  "../../mary/lib/marytts-runtime-5.2-jar-with-dependencies.jar"
 ) ++: cp)
--- src/main/scala/speaker/Say.scala
+++ src/main/scala/speaker/Say.scala
@@ -1,22 +1,42 @@
 package speaker
 
-import java.util.Locale
 
-import marytts.LocalMaryInterface
+import akka.actor.{Actor, ActorLogging, Props}
+import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.server.{HttpApp, Route}
+import speaker.WebServer.Speaker.Say
 
-object Say extends App {
-  println("Loading")
-  val marytts = new LocalMaryInterface
-  marytts.setLocale(Locale.GERMAN)
 
-  val audioF = marytts.generateAudio(",, Guten morgen... Es ist 7 Uhr 30 .. Aufstehen . Ein schöner neuer Tag erwartet Dich")
-  println("Say something")
 
-  val audio = new AudioPlayer(audioF, 0, _ => {
-    println("sound ended")
-  })
-  audio.gain(50)
-  audio.play()
+object WebServer extends HttpApp {
 
-  Thread.sleep(7000)
+  class Speaker extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case Say(text) => println("saying "+text)
+    }
+  }
+
+  object Speaker {
+    case class Say(text:String)
+  }
+
+  lazy val speaker = systemReference.get.actorOf(Props[Speaker])
+
+  override def routes: Route =
+    path("say") {
+      post {
+        entity(as[String]) { body =>
+          complete{
+            speaker ! Say(body)
+            HttpEntity.Empty
+          }
+        }
+      }
+    }
+}
+
+
+object Process extends App {
+  println("Speaker version 0.0.0")
+  WebServer.startServer("localhost", 9100)
 }
